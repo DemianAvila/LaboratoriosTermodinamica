48845127e34d680e48f5f9bff351835bd7a26259
Demian Avila <demian_avila@comunidad.unam.mx>
2022-04-18 01:14:31 -0500

    cancelar el cambio de clrf a lf

---
+++ frontend/frontend-labs/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <div class="texto">
+      <div id="backg"
+      class="
+      bg-[url('./img/movil.svg')]
+      lg:bg-[url('./img/escritorio.svg')]
+      bg-cover
+      bg-no-repeat
+      h-screen">
+        <div>
+          <router-view/>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Zen+Kurenaido&display=swap');
+.texto{
+font-family: 'Zen Kurenaido', sans-serif;
+}
+</style>
---
+++ frontend/frontend-labs/src/views/HomeView.vue
@@ -0,0 +1,84 @@
+<template>
+  <div class="p-5">
+    <h1 class="text-sky-100
+    text-center
+    text-4xl
+    lg:text-7xl
+    col-span-3">
+        Prácticas
+    </h1>
+    <a href="http://www.google.com"
+    id="google-button"
+    class="flex
+    justify-center
+    grid
+    grid-cols-2
+    lg:row-start-4
+    row-start-5
+    max-h-10
+    lg:col-start-2">
+        <div class="col-span-2
+        text-sky-100
+        text-center
+        text-xl
+        border-2
+        border-sky-100
+        rounded-md 
+        mx-20
+        p-2
+        grid
+        grid-flow-col
+        bg-red-600
+        hover:bg-amber-400">
+            <h1 class="border-r-2
+            border-sky-100">
+                <i class="fab
+                fa-google"></i>
+            </h1>
+            <h1>
+                Ingresa con Google
+            </h1>        
+        </div>
+    </a>
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+  return {
+    practicas: [
+      {nombre: "Práctica 1 - Presión",
+      submenus: ["Previo", "Desarrollo"],
+      status: "En proceso",
+      progreso: "50%",
+      disponible: "True"},
+      {nombre: "Práctica 2 - Temperatura",
+      submenus: ["Previo", "Desarrollo"],
+      status: "En proceso",
+      progreso: "50%",
+      disponible: "True"},
+      {nombre: "Práctica 3 - Calor",
+      submenus: ["Previo", "Desarrollo"],
+      status: "No iniciado",
+      progreso: "0%",
+      disponible: "True"},
+      {nombre: "Práctica 4 - Capacidad térmica específica de metales",
+      submenus: ["Previo", "Desarrollo"],
+      status: "No iniciado",
+      progreso: "0%",
+      disponible: "False"},
+      {nombre: "Práctica 5 - Conversión de trabajo en calor",
+      submenus: ["Previo", "Desarrollo"],
+      status: "Finalizado",
+      progreso: "100%",
+      disponible: "True"}
+      ]
+    }
+  },
+  name: 'Predeterminada',
+  components: { }
+}
+</script>
+
+
