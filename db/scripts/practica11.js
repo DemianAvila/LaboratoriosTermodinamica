9cb0bf454a786d6ea714855d3a2b5adc5ce13122
Demian Avila <demianavilar@gmail.com>
2023-01-16 13:01:02 -0600

    feat: transcribed all the questions

--- db/scripts/practica11.js
+++ db/scripts/practica11.js
@@ -9,19 +9,19 @@ db.cuestionario_previo.insertMany([
     answer_type: "textarea"
   },
   {
-    practica_id: practica10,
+    practica_id: practica11,
     question_type: "open",
     question: `¿Qué es la calidad de un vapor y qué valores puede tener?`,
     answer_type: "textarea"
   },
   {
-    practica_id: practica10,
+    practica_id: practica11,
     question_type: "open",
     question: `¿Qué es una caldera?`,
     answer_type: "textarea"
   },
   {
-    practica_id: practica10,
+    practica_id: practica11,
     question_type: "open",
     question: `¿Cómo se expresa la entalpia de una mezcla líquido-vapor en equilibrio, en\
     términos de las entalpias del líquido y del vapor saturados, para el caso de un\
@@ -29,15 +29,89 @@ db.cuestionario_previo.insertMany([
     answer_type: "textarea"
   },
   {
-    practica_id: practica10,
+    practica_id: practica11,
     question_type: "many",
     question: `¿En qué dispositivos se llevan a cabo procesos de estrangulamiento?`,
     answer_type: "textarea"
   },
   {
-    practica_id: practica1,
+    practica_id: practica11,
     question_type: "attachment",
     question:
-      "Use el método de los mínimos cuadrados para obtener el modelo matemático de la presión en función de la altura y determine el valor de la presión atmosférica a una altura de 0.50 [m]Hg.",
+      `Dibuje un esquema aproximado de la curva de inversión`,
+  },
+  {
+    practica_id: practica11,
+    question_type: "open",
+    question: `¿Por que se le llama curva de inversión?`,
+    answer_type: "textarea"
+  },
+  {
+    practica_id: practica11,
+    question_type: "many",
+    question: `Mencione algunos tipos de válvulas`,
+    answer_type: "textarea"
+  },
+  {
+    practica_id: practica11,
+    question_type: "multiple_question",
+    question: "Investigue los valores de la entalpia de evaporización a las siguientes presiones:",
+    fields: [
+      {
+        _id: ObjectId(),
+        field: "760 [mm] Hg",
+        type: "textarea",
+      },
+      {
+        _id: ObjectId(),
+        field: "540 [mm] Hg",
+        type: "textarea",
+      },
+      {
+        _id: ObjectId(),
+        field: "400 [mm] Hg",
+        type: "textarea",
+      },
+    ],
+  },
+  {
+    practica_id: practica11,
+    question_type: "multiple_question",
+    question: `Utilizando tablas de vapor de agua, calcule la entalpia de mezcla, si la fracción
+    másica del vapor es igual a 0.8 y el sistema se encuentra a una presión de 30
+    [psig]. Exprese el resultado en`,
+    fields: [
+      {
+        _id: ObjectId(),
+        field: "Unidades del Sistema Internacional",
+        type: "textarea",
+      },
+      {
+        _id: ObjectId(),
+        field: "Unidades del Sistema Inglés",
+        type: "textarea",
+      },
+    ],
+  },
+  {
+    practica_id: practica11,
+    question_type: "choice",
+    question: `¿Cómo se interpretan los siguientes valores del coeficiente de Joule-Thomson
+    (μJT) con respecto a la temperatura?`,
+    options: [
+      {
+        option: "a",
+        answer: "potitivo"
+      },
+      {
+        option: "b",
+        answer: "negativo"
+      },
+      {
+        option: "c",
+        answer: "igual a cero"
+      }
+    ]
   },
 ])
+
---
+++ db/scripts/practica12.js
@@ -0,0 +1,118 @@
+//INSERTAR CUESTIONARIO PREVIO
+db = db.getSiblingDB("laboratorio_info");
+practica12 = 12;
+db.cuestionario_previo.insertMany([
+  {
+    practica_id: practica12,
+    question_type: "open",
+    question: `¿Qué es una máquina térmica?`,
+    answer_type: "textarea"
+  },
+  {
+    practica_id: practica12,
+    question_type: "open",
+    question: `¿Qué es una bomba de calor?`,
+    answer_type: "textarea"
+  },
+  {
+    practica_id: practica12,
+    question_type: "attachment",
+    question:
+      `Dibuje un esquema de una máquina térmica y de una bomba de calor,
+      indicando los flujos energéticos involucrados.`,
+  },
+  {
+    practica_id: practica12,
+    question_type: "open",
+    question: `¿Qué es un compresor?`,
+    answer_type: "textarea"
+  },
+  {
+    practica_id: practica11,
+    question_type: "many",
+    question: `¿Qué tipos de compresores se utilizan
+    industrialmente?`,
+    answer_type: "textarea"
+  },
+  {
+    practica_id: practica12,
+    question_type: "open",
+    question: `Investigue las características del refrigerante R-134a.`,
+    answer_type: "textarea"
+  },
+  {
+    practica_id: practica12,
+    question_type: "open",
+    question: `Escriba la ecuación de la primera ley de la termodinámica aplicada a un ciclo`,
+    answer_type: "math"
+  },
+  {
+    practica_id: practica11,
+    question_type: "many",
+    question: `Explique cada uno de los términos de la ecuación 
+    de la primera ley de la termodinámica`,
+    answer_type: "textarea"
+  },
+  {
+    practica_id: practica12,
+    question_type: "open",
+    question: `Con base en la primera ley de la Termodinámica escriba la ecuación que
+    permita calcular el trabajo que requiere el compresor adiabático para su
+    funcionamiento.`,
+    answer_type: "math"
+  },
+  {
+    practica_id: practica12,
+    question_type: "open",
+    question: `Escriba la ecuación del balance de entropía en un compresor`,
+    answer_type: "math"
+  },
+  {
+    practica_id: practica11,
+    question_type: "many",
+    question: `Explique cada uno de los términos de la ecuación 
+    del balance de entropía en un compresor`,
+    answer_type: "textarea"
+  },
+  {
+    practica_id: practica11,
+    question_type: "multiple_question",
+    question: "Investigue y explique qué es:",
+    fields: [
+      {
+        _id: ObjectId(),
+        field: "La eficiencia",
+        type: "textarea",
+      },
+      {
+        _id: ObjectId(),
+        field: "La eficiencia mecánica",
+        type: "textarea",
+      },
+      {
+        _id: ObjectId(),
+        field: "La eficiencia térmica",
+        type: "textarea",
+      },
+      {
+        _id: ObjectId(),
+        field: "La eficiencia adiabática",
+        type: "textarea",
+      },
+    ],
+  },
+  {
+    practica_id: practica12,
+    question_type: "open",
+    question: `Investigue y escriba la ecuación que se aplica para calcular la eficiencia
+    adiabática en un compresor.`,
+    answer_type: "math"
+  },
+  {
+    practica_id: practica12,
+    question_type: "attachment",
+    question:
+      `Dibuje los diagramas termodinámicos (s, T), (h, P) y (v, P) para una máquina
+      de refrigeración.`,
+  },
+])
