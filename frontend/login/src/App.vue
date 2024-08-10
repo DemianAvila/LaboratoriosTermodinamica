03773a7566c3d05f7a7b3ee8a6fb39c966a82aa1
demianavilar <demianavilar@demianavilar.arinnovations.com.mx>
2024-08-10 00:05:32 -0600

    feat: refactoring code into microservices

---
+++ frontend/login/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="container">
+    <NavBar />
+  </div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+import NavBar from "./components/NavBar.vue";
+
+@Options({
+  components: {
+    NavBar,
+  },
+})
+export default class App extends Vue {}
+</script>
+
+<style>
+body {
+  width: 100vw;
+  height: 100vh;
+  overflow: hidden;
+  color: white;
+  background-image: url(@/assets/img/bg.svg);
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-attachment: scroll;
+  margin: 0;
+  padding: 0;
+}
+
+.container {
+  display: flex;
+  width: 100vw;
+  height: 100vh;
+}
+</style>
---
+++ frontend/login/src/components/NavBar.vue
@@ -0,0 +1,32 @@
+<template>
+  <nav class="navbar">
+    <div class="size">a</div>
+    <div class="size">b</div>
+  </nav>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from "vue-class-component";
+
+@Component
+export default class NavBar extends Vue {
+  hello = "fuck";
+}
+</script>
+
+<style>
+.navbar {
+  width: 100%;
+  height: 10%;
+  min-height: 5em;
+  background-color: red;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  padding: 10px;
+}
+.size {
+  height: 100%;
+  width: 50%;
+}
+</style>
